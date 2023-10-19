6e8eb0c2c54942d915cc651939b3a47eefa5a07c
Olga-ampilogova <141365234+Olga-ampilogova@users.noreply.github.com>
2023-10-19 09:47:40 +0200

    Update hometask

--- task-3.js
+++ task-3.js
@@ -1,9 +1,16 @@
-function getElementWidth(content, padding, border) {
-    let contentInt = Number.parseFloat(content);
-    let paddingInt = Number.parseFloat(padding);
-    let borderInt = Number.parseFloat(border);
-    return  contentInt + paddingInt * 2 +borderInt * 2;
+function checkForSpam(message) {
+    const spamMess = "spam";
+    const saleMess = "sale";
+    let lowCaseMsg = message.toLowerCase();
+    return lowCaseMsg.includes(saleMess) || lowCaseMsg.includes(spamMess) ? true : false;
 }
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+       
+
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
